bfba5c919eb53cecaec1170a2a785c81d988fb2c
Unknown <unknown@example.com>
2020-11-11 22:20:01 -0500

    TareaUseCase: +MoveTarea move

--- src/main/java/com/jhw/module/admin/kanban/core/usecase_def/TareaUseCase.java
+++ src/main/java/com/jhw/module/admin/kanban/core/usecase_def/TareaUseCase.java
@@ -2,6 +2,7 @@ package com.jhw.module.admin.kanban.core.usecase_def;
 
 import com.clean.core.app.usecase.CRUDUseCase;
 import com.jhw.module.admin.kanban.core.domain.ColumnaProyectVolatile;
+import com.jhw.module.admin.kanban.core.domain.MoveTarea;
 import com.jhw.module.admin.kanban.core.domain.TareaDomain;
 import java.util.List;
 
@@ -9,5 +10,5 @@ public interface TareaUseCase extends CRUDUseCase<TareaDomain> {
 
     public List<TareaDomain> findByColumnaProyecto(ColumnaProyectVolatile colProy) throws Exception;
 
-    public TareaDomain move(Integer idTarea, Integer idColumna) throws Exception;
+    public TareaDomain move(MoveTarea move) throws Exception;
 }
