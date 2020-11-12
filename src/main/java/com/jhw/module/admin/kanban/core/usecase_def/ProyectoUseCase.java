f74fe0a7d3ef900c8ec5fc4bbe7f8a91d3103bc7
Unknown <unknown@example.com>
2020-11-11 22:20:10 -0500

    ProyectoUseCase: -comentarios

--- src/main/java/com/jhw/module/admin/kanban/core/usecase_def/ProyectoUseCase.java
+++ src/main/java/com/jhw/module/admin/kanban/core/usecase_def/ProyectoUseCase.java
@@ -5,19 +5,4 @@ import com.jhw.module.admin.kanban.core.domain.ProyectoDomain;
 
 public interface ProyectoUseCase extends CRUDUseCase<ProyectoDomain> {
 
-    /**
-     * Actualiza todas las ramas contra el repo online
-     *
-     * @param proyecto
-     * @throws Exception
-     *
-    public void updateRemote(ProyectoDomain proyecto) throws Exception;
-
-    /**
-     * Revisa si un proyecto tiene al menos un remote
-     *
-     * @param proyecto
-     * @throws Exception
-     *
-    public boolean hasRemote(ProyectoDomain proyecto);*/
 }
